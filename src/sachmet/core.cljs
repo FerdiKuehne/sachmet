f74df77ea21da646819ecbba42c2b56c324f9d27
FerdiKuehne <ferdi.kuehne@gmx.net>
2017-07-10 12:24:05 +0200

    adding a parser to core.cljs

--- src/sachmet/core.cljs
+++ src/sachmet/core.cljs
@@ -8,6 +8,32 @@
 ;;state safe via global atom
 (def app-state (atom {:count 0}))
 
+
+;;parser Read Function. signature: [env key params]
+;;query expressions = always a vector -> parsing it = a map
+(defn read
+  [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ v] (find st key)]
+      {:value v}
+      {:value :not-found})))
+
+;;parser mutation function. signature: identical to read function 
+;; :key vector -> what reaad operation should follow the mutation
+;; :action = function with no argumentts 
+
+(defn mutate
+  [{:keys [state] :as env} key params]
+  (cond
+        (= 'increment key) {:value {:keys [:count]} :action #(swap! state update-in [:count] inc)}
+        (= 'decrement key) {:value {:keys [:count]} :action #(swap! state update-in [:count] dec)}
+        (= 'reset key) {:value {:keys [:count]} :action #(reset! state)}
+        :else {:value :not-found}))
+
+;;parsing involves 2 kinds of expressions reads and mutations
+;;takes a query expression and evals it using the provided read and mutate implementations
+;;(def my-parser (om/parser {:read read}))
+
 ;;syntax for declaring om Objects (JavaScriptClass) without parameterizing
 (defui intro
   Object
@@ -25,31 +51,33 @@
 
 ;;new syntax for state components
 (defui Counter
+  static om/IQuery
+  (query [this]
+    [:count])
   Object
   (render [this]
     (let [{:keys [count]} (om/props this)]
       (dom/div nil
         (dom/span nil (str "Number: " count "  "))
         (dom/button
-          #js {:onClick
-               (fn [e]
-                 (swap! app-state update-in [:count] inc))}
+         #js {:onClick
+              (fn [e] (om/transact! this '[(increment)]))}
           "inc")
         (dom/button
-          #js {:onClick
-               (fn [e]
-                 (swap! app-state update-in [:count] dec))}
+         #js {:onClick
+              (fn [e] (om/transact! this '[(decrement)]))}
           "dec")
         (dom/button
           #js {:onClick
-               (fn [e]
-                 (reset! app-state))}
+                 (fn [e] (om/transact! this '[(reset)]))}
           "reset")
         ))))
 
 
 (def reconciler
-  (om/reconciler {:state app-state}))
+  (om/reconciler
+   {:state app-state
+    :parser (om/parser {:read read :mutate mutate})}))
 
 ;;producing factory from objects
 (def introGods (om/factory intro))
